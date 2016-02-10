555ff07c7a3e0a4b68e5beb4b8b9f81b4700da0d
Alex Fallenstedt and erik tolentino <tolentino3575@gmail.com>
2016-02-10 11:47:53 -0800

    push final

--- app/app.php
+++ app/app.php
@@ -9,7 +9,7 @@
         $_SESSION['list_of_places'] = array();
     }//The first line starts our session. The next line then checks it for the key 'list_of_tasks' using the empty function, which returns true if there is no value stored in the variable. If the key doesn't exist, we create it and set it to an empty array.
 
-    $app = new Siliex\Application();
+    $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
@@ -20,12 +20,15 @@
     });
 
     $app->post("/places", function() use ($app) { //links to form on places.html.twig
-        $place = new Places($_POST['cityname']);
+        $place = new Places($_POST['cityname']);//cityname must be the same on the /places form in places.html.twig
         $place->save();
-        return $app['twig']->render('output_places.html.twig', array('newplace' => $place));
+        return $app['twig']->render('output_places.html.twig', array('newplace' => $place)); //newplace array referenced in output_places.html.twig
     });
 
-    
+    $app->post("/delete_places", function() use ($app) {
+        Places::deleteAll();
+        return $app['twig']->render('delete_places.html.twig');
+    });
 
-
+    return $app;
  ?>
